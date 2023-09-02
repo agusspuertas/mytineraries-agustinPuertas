b651234f628eebe9dd774b3e112b222c4c03df32
agusspuertas <agustinnpuertas@gmail.com>
2023-09-02 02:47:33 -0300

    chages

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -38,7 +38,7 @@ function Detail() {
       </article>
     </section>
 
-    <section id='bububu' className='flex justify-center items-center '>
+    <section className='flex justify-center items-center '>
       <div className='w-2/6 h-56 justify-center items-center mb-10 bg-gray-200 shadow-xl shadow-gray-600 rounded-2xl'>
         
       </div>
