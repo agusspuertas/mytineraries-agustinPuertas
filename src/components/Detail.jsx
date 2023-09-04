280f52a073f387155d28fc81916bc68eb4a27f28
agusspuertas <agustinnpuertas@gmail.com>
2023-09-04 09:58:44 -0300

    changes

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -12,51 +12,55 @@ function Detail() {
   const { id } = useParams()
   const [itineraries, setItineraries] = useState();
 
-    console.log(itineraries)
+  console.log(itineraries)
 
   useEffect(() => {
+
     axios.get(`http://localhost:7000/api/cities/${id}`)
       .then(response => setCity(response.data.city))
       .catch(err => console.log(err))
 
-      axios.get(`http://localhost:7000/api/itineraries?cityId=${id}`)
-      .then(response => setItineraries(response.data.itineraries))
+    axios.get(`http://localhost:7000/api/itineraries/city/${id}`)
+      .then(response => {
+        console.log(response)
+        setItineraries(response.data)})
       .catch(err => console.log(err))
+
 
   }, []);
 
-  
+
 
   return (
 
     <main className='flex flex-col'>
 
-    <section className=' mt-10 flex justify-center items-start mb-10 '>
+      <section className=' mt-10 flex justify-center items-start mb-10 '>
 
-      <article className='flex shadow-xl shadow-gray-600 rounded-r-2xl rounded-l-lg'>
-        <img src={city.imagen} alt="img" className='h-[60vh] w-[40vw] object-cover rounded-l-lg' />
-        <div className='w-80 flex flex-col rounded-r-2xl bg-gray-300 justify-center items-center '>
-          <div  className='shadow-md rounded-xl mx-5 py-2  shadow-neutral-400'>
-            <h2 className='text-4xl text-center font-bold'>{city.nombre}</h2>
-            <h3 className='text-4xl text-center font-light'>{city.pais}</h3>
-            <div className='mt-10 mx-4  p-3'>
-              <p className="mb-3 font-semibold text-sm dark:text-gray-400">{city.descripcion}</p>
+        <article className='flex shadow-xl shadow-gray-600 rounded-r-2xl rounded-l-lg'>
+          <img src={city.imagen} alt="img" className='h-[60vh] w-[40vw] object-cover rounded-l-lg' />
+          <div className='w-80 flex flex-col rounded-r-2xl bg-gray-300 justify-center items-center '>
+            <div className='shadow-md rounded-xl mx-5 py-2  shadow-neutral-400'>
+              <h2 className='text-4xl text-center font-bold'>{city.nombre}</h2>
+              <h3 className='text-4xl text-center font-light'>{city.pais}</h3>
+              <div className='mt-10 mx-4  p-3'>
+                <p className="mb-3 font-semibold text-sm dark:text-gray-400">{city.descripcion}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
-    </section>
+        </article>
+      </section>
 
       <section>
         {
-            itineraries?.length > 0 ?
+          itineraries?.length > 0 ?
             itineraries?.map((itinerary) => {
-                return (
-                   <ItineraryCard nombre={itinerary.nombre} nombreUsuario={itinerary.nombreUsuario}  usuarioFoto={itinerary.usuarioFoto} duracion={itinerary.duracion} precio={itinerary.precio} likes={itinerary.likes} hashtags={itinerary.hashtags} comentarios={itinerary.comentarios} />
-                )
+              return (
+                <ItineraryCard nombre={itinerary.nombre} nombreUsuario={itinerary.nombreUsuario} usuarioFoto={itinerary.usuarioFoto} duracion={itinerary.duracion} precio={itinerary.precio} likes={itinerary.likes} hashtags={itinerary.hashtags} comentarios={itinerary.comentarios} />
+              )
             })
-            : <h1 className='text-4xl text-center'>No itineraries found</h1>
-    }
+            : <h1 className='text-4xl font-bold text-center'>No itineraries found for this city</h1>
+        }
       </section>
 
     </main>
